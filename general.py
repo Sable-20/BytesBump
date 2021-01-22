c335d95f8ba58911ac8cd43f698d919d119ac752
Sable no <72171476+Sable-20@users.noreply.github.com>
2021-01-22 13:43:08 +0000

    Added error handler file
    Added a more extensive error handler and removed old one

---
+++ general.py
@@ -0,0 +1,55 @@
+import discord, asyncio
+from Admin.admin import Files
+from discord.ext import tasks
+
+commands = discord.ext.commands
+emoji = Files.emoji
+
+class General(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    self.update_status.start()
+
+  @tasks.loop(seconds=60)
+  async def update_status(self):
+    print("Updating users!")
+    await asyncio.sleep(2)
+    return await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f"=help | {len(self.bot.users)} Users"))
+
+  @commands.command()
+  async def support(self, ctx):
+    appinfo = await self.bot.application_info()
+    return await ctx.send(embed=discord.Embed(
+      description=f"**BytesBump** was developed by the [BytesToBits](https://discord.gg/r2rAUJZ) development team, and it is owned by `{appinfo.team.owner}`!\n\n[You can join the support server here!](https://discord.gg/8akycDh)",
+      color=discord.Color.blue()
+    )
+    .set_footer(text="Powered by • BytesBump")
+    .set_thumbnail(url=self.bot.user.avatar_url_as(static_format="png")))
+
+  @commands.command()
+  async def invite(self, ctx):
+    return await ctx.send(embed=discord.Embed(
+      description=f"You can invite **BytesBump** by [Clicking Here!](https://discord.com/api/oauth2/authorize?client_id=776077253473337366&permissions=347137&scope=bot)",
+      color=discord.Color.blue()
+    )
+    .set_thumbnail(url=self.bot.user.avatar_url_as(static_format="png")))
+  
+  @commands.command(aliases=["about"])
+  async def info(self, ctx):
+    appinfo = await self.bot.application_info()
+    owners = ', '.join([str(i) for i in appinfo.team.members])
+    return await ctx.send(embed=discord.Embed(
+      title=f"{self.bot.user.name} | Information",
+      color=discord.Color.blurple()
+    )
+    .add_field(name="Version", value=Files.config("main", "version"))
+    .add_field(name="Library", value="Discord.py")
+    .add_field(name="Prefix", value=Files.config("main", "prefix"))
+    .add_field(name="Guilds", value=len(self.bot.guilds))
+    .add_field(name="Users", value=len(self.bot.users))
+    .add_field(name="Support Server", value=f"[BytesToBits](https://discord.gg/r2rAUJZ)")
+    .set_footer(text=f"Owned by {owners}")
+    .set_thumbnail(url=self.bot.user.avatar_url_as(static_format="png")))
+
+def setup(bot):
+  bot.add_cog(General(bot))
---
+++ manage.py
@@ -0,0 +1,93 @@
+import discord
+import asyncio
+from discord.ext import commands
+from asyncio import sleep
+
+def convert(time): #makes life easier and looks better overall
+    day = time // (24 * 3600)
+    time = time % (24 * 3600)
+    hour = time // 3600
+    time %= 3600
+    minutes = time // 60
+    time %= 60
+    seconds = time
+    return "%d:%d:%d:%d" % (day, hour, minutes, seconds)
+
+class Events(commands.Cog):
+    def __init__(self, bot, *args, **kwargs):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        global time
+        if isinstance(error, commands.CommandNotFound):
+            pass #this is just to make sure it doesnt send a message when command not found
+        if isinstance(error, commands.CommandOnCooldown):
+            time = error.retry_after
+            time = convert(time)
+            x = time.split(":") #this fuckwad of an if statement checks hours minutes seconds, etc
+            if x[1] != '0' and x[2] != '0':
+                if x[1] == 1:
+                    message = f'Retry this command after **{x[1]}** hour(s) and **{x[2]}** minutes!'
+                else:
+                    message = f'Retry this command after **{x[1]}** hour(s) and **{x[2]}** minutes!'
+            elif x[1] == '0' and x[2] != '0' and x[3] != 0:
+                message = f'Retry this command after **{x[2]}** minutes and **{x[3]}** seconds!'
+            elif x[3] != '0' and x[1] == '0' and x[2] == '0':
+                message = f'Retry this command after **{x[3]}** seconds!'
+            msg = await ctx.send(message)
+            await sleep(3)
+            await msg.delete()
+        if isinstance(error, commands.MissingRequiredArgument):
+            msg = await ctx.send("**You have made an error.**\n\n{}".format(error.param))
+            await sleep(3) #missing params, can get rid of
+            await msg.delete()
+        if isinstance(error, commands.TooManyArguments):
+            msg = await ctx.send('You have given too many args.\nPlease use the command as directed.')
+            await sleep(3) #to many args
+            await msg.delete()
+        if isinstance(error, commands.BotMissingPermissions):
+            msg = await ctx.send('I am missing permissions.')
+            await sleep(3) #bot is missing permissions
+            await msg.delete()
+        if isinstance(error, commands.ExtensionAlreadyLoaded):
+            msg = await ctx.send('The cog {} is already loaded.'.format(error.args[0]))
+            await sleep(3) #cog is already loaded
+            await msg.delete()
+        if isinstance(error, commands.MissingPermissions):
+            msg = await ctx.send('You need **{}** perms to complete this actions.'.format(error.missing_perms[0]))
+            await sleep(3) #user missing perms
+            await msg.delete()
+        if isinstance(error, commands.BotMissingAnyRole):
+            msg = await ctx.send('**Woops!**\n\nLooks like i am missing the {} role.'.format(error.missing_role))
+            await sleep(3) #bot missing role
+            await msg.delete()
+        if isinstance(error, commands.CheckAnyFailure):
+            msg = await ctx.send('An unknown error has occured.')
+            await sleep(3) #unknown
+            await msg.delete()
+        if isinstance(error, commands.errors.NSFWChannelRequired):
+            msg = await ctx.send('You must use this command in a channel marked as **NSFW**.')
+            await sleep(3) #nsfw only
+            await msg.delete()
+        if isinstance(error, commands.errors.NotOwner):
+            msg = await ctx.send('Only **{}** can use this command.'.format(ctx.guild.owner))
+            await sleep(3) #owner only commmands
+            await msg.delete()
+        if isinstance(error, commands.errors.NoPrivateMessage):
+            msg = await ctx.send("The user has blocked me or has the DM's closed.")
+            await sleep(3) #no dms
+            await msg.delete()
+        if isinstance(error, discord.ext.commands.DisabledCommand):
+            msg = await ctx.send('This command is disabled.')
+            await sleep(3) #disabled
+            await msg.delete()
+        if isinstance(error, discord.errors.Forbidden):
+            msg = await ctx.send('I do not have permissions for this command!')
+            await sleep(3) #frobidden from using command
+            await msg.delete()
+        #uncomment to log every error
+        # print(error)
+
+def setup(bot):
+    bot.add_cog(Events(bot))
